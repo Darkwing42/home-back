0654a60ec4bb13eb41c8f20b211b3c961ce9ad4d
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-10-09 16:12:58 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 import os
+#https://scotch.io/tutorials/build-a-restful-api-with-flask-the-tdd-way
 
 from home_back import app, db
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+#app = create_app(config_name=))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+	manager.run()
